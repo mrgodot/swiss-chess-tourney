9b44f9eeaf9752acef46ef2c5162e207e8386d19
zre <zach.r.ernst@gmail.com>
2024-08-04 22:22:45 -0700

    move game creation into class method

--- tournament/game.py
+++ tournament/game.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from random import shuffle
 
 from attrs import define, field
 import pandas as pd
 
-from tournament.utils import Outcome
+from tournament.lichess import create_lichess_challenge
+from tournament.player import Player
+from tournament.utils import Outcome, expires_at_timestamp, timestamp_to_datetime
 
 
 @define
@@ -24,6 +27,40 @@ class Game:
             white=series['White'],
             black=series['Black'],
             outcome=Outcome(series['Outcome']) if series['Outcome'] != '' else None)
+
+    @classmethod
+    def create(cls, round_num: str, players: tuple[Player], lichess_api_token: str,
+               testing: bool = False, days_until_expired: int = 7,
+               **kwargs):
+
+        player_pair = [players[0], players[1]]
+
+        # randomize side if not bye
+        if not players[1].is_bye:
+            shuffle(player_pair)
+
+        expires_at = expires_at_timestamp(days_until_expired)
+        expires_at_datetime = timestamp_to_datetime(expires_at)
+
+        if testing:
+            game_link = 'https://lichess.org/'
+        else:
+            game_link = create_lichess_challenge(
+                round_num=round_num,
+                white_player=player_pair[0],
+                black_player=player_pair[1],
+                api_token=lichess_api_token,
+                **kwargs)
+
+        return cls(
+            round_num=int(round_num),
+            white=player_pair[0].name,
+            black=player_pair[1].name,
+            score_delta=player_pair[0].score - player_pair[1].score,
+            games_played=players[0].match_count(players[1].name),
+            match_link=game_link,
+            expires=expires_at_datetime,
+            outcome=Outcome.white if players[1].is_bye else None)
 
     @property
     def bye(self):
